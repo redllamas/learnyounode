45ab2f952aba7986fd646dda083bf965678a1b2c
redllamas <sam@jetsets.jp>
2015-01-24 22:51:36 +0900

    added http-file-server.js

--- http-file-server.js
+++ http-file-server.js
@@ -1,18 +1,14 @@
-var fs = require('fs');
-var http = require('http');
+var fs         = require('fs');
+var http       = require('http');
 var portNumber = process.argv[2];
-var filePath = process.argv[3];
+var filePath   = process.argv[3];
 
-var server = http.createServer(function callback(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('okay');
 
-  var stream = fs.createReadStream(filePath);
+var server     = http.createServer(function callback(req, res) {
+  res.writeHead(200, {'content-type': 'text/plain'});
+  var src      = fs.createReadStream(filePath);
+  src.pipe(res);
 
 });
 
 server.listen(portNumber);
-
-// req = request, which will fetch properties and query-string for the file we need 
-// res = response, which sends data to the client, header and body
-// req and res 
